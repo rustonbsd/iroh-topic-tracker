3e3cc22bf2f23c46a62b64cdf2093b715b345747
rustonbsd <rustonbsd@mailfence.com>
2025-01-16 18:16:55 +0100

    exposed GossipAutoDiscovery struct

--- src/integrations/mod.rs
+++ src/integrations/mod.rs
@@ -2,5 +2,5 @@ mod iroh_gossip_int;
 
 #[cfg(feature = "iroh-gossip-auto-discovery")]
 pub mod iroh_gossip {
-    pub use super::iroh_gossip_int::{AutoDiscoveryBuilder, AutoDiscoveryGossip, AutoDiscoveryNew};
+    pub use super::iroh_gossip_int::{AutoDiscoveryBuilder, AutoDiscoveryGossip, AutoDiscoveryNew,GossipAutoDiscovery};
 }
